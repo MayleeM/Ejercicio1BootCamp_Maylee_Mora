6f93076e7d453c368cca4f4371b40e36f575b038
Maylee Mora <maylee.mora@tsoftglobal.com>
2024-09-09 11:32:08 -0300

    Se sube primer ejercicio

---
+++ src/ejercicios/ejercicio1.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+    //1. Suma de dos números
+    //Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+    //Variables: a = 10, b = 20.
+
+        int a = 10;
+        int b = 20;
+
+        int suma = a+b;
+
+        System.out.println("Inprime el resultado de la suma: " + suma);
+    }
+}
