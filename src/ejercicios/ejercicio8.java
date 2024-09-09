324a9dca7678b18a5c3a964c819f291814305fd0
Maylee Mora <maylee.mora@tsoftglobal.com>
2024-09-09 12:25:01 -0300

    Se sube octavo ejercicio

---
+++ src/ejercicios/ejercicio8.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class ejercicio8 {
+
+    public static void main(String[] args) {
+    // 8. Conversión de temperatura
+    //•	Descripción: Dado que la temperatura es c = 25 grados Celsius, conviértela a Fahrenheit usando la fórmula:
+    //•	Variables: c = 25.
+
+        double c = 25;
+
+        double f = (c * 9/5) + 32;
+
+        System.out.println(c + " grados Celsius equivalen a " + f + " grados Fahrenheit");
+
+    }
+}
