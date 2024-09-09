0b2c7f2f27c38ca14ed745f843b3de55fa733ca4
Maylee Mora <maylee.mora@tsoftglobal.com>
2024-09-09 12:02:41 -0300

    Se sube sexto y septimo ejercicio

---
+++ src/ejercicios/ejercicio6.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+    //  6. Promedio de tres números
+    //•	Descripción: Dados los números a = 4, b = 9 y c = 13, calcula el promedio de los tres.
+    //•	Variables: a = 4, b = 9, c = 13.
+
+        int a = 4;
+        int b = 9;
+        int c = 13;
+
+        double promedio = (a + b + c) / 3.0;
+
+        System.out.println("El promedio de los tres números es: " + promedio);
+    }
+}
---
+++ src/ejercicios/ejercicio7.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+public class ejercicio7 {
+
+    public static void main(String[] args) {
+    // 7. Área de un triángulo
+    //•	Descripción: Dadas la base b = 5 y la altura h = 8 de un triángulo, calcula su área usando la fórmula:
+    //
+    //•	Variables: b = 5, h = 8.
+
+        double b = 5;
+        double h = 8;
+
+        double area = (b * h) / 2;
+
+        System.out.println("El área del triángulo es: " + area);
+    }
+}
+
+
