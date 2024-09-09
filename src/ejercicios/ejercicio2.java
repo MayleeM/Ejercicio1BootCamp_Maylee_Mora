9edb871d6b3c6e333bd92b05653f843420794d56
Maylee Mora <maylee.mora@tsoftglobal.com>
2024-09-09 11:38:53 -0300

    Se sube segundo ejercicio

---
+++ src/ejercicios/ejercicio2.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+
+    //2. Producto de dos números
+    //Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
+    //Variables: a = 7, b = 8.
+
+        int a = 7;
+        int b = 8;
+
+        int resultado = a*b;
+
+        System.out.println("El resultado de la multiplicación es: " + resultado);
+    }
+}
