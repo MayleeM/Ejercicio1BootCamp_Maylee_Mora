66fae5b4773b31fcf760b664c14d20895063e5d0
Maylee Mora <maylee.mora@tsoftglobal.com>
2024-09-09 15:31:06 -0300

    Se sube ejercicio 14

---
+++ src/ejercicios/ejercicio14.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class ejercicio14 {
+    public static void main(String[] args) {
+    //14. Conversión de decimal a binario
+        //•	Descripción: Dado el número decimal a = 45, conviértelo a binario.
+        //•	Variables: a = 45.
+
+        int a = 45;
+        String binario = Integer.toBinaryString(a);
+        System.out.println(binario); // 1000
+
+    }
+}
